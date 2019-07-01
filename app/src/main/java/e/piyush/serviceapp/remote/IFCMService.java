93becfda7b82ae2282ef6707be0281248355185d
Ayush shrivastava <ayushshrivastava675@gmail.com>
2019-07-02 04:20:32 +0530

    Update IFCMService.java

--- app/src/main/java/e/piyush/serviceapp/remote/IFCMService.java
+++ app/src/main/java/e/piyush/serviceapp/remote/IFCMService.java
@@ -10,8 +10,7 @@ import retrofit2.http.POST;
 public interface IFCMService {
     @Headers({
             "Content-Type:application/json",
-                 "Authorization:key=AAAAVZcTpbk:APA91bFTFPWfxtew8YsN-kTZbLXEdx8MfyIwropdTt97LLE5MfXASOcKe8K5NnLz-_EutX2-4gTWDGRDKyn98vzStivQ70_toSza2CWdOMqBQS2v9MEE6pU5yet1SiwLux-yKXgGDD9y"
-
+                 "Authorization:key=your key
 
 
     })
